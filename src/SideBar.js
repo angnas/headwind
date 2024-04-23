30e8a45702cd27271ee1ffad18fc7e8bf4c7e647
Angela Naseri <f.naseri1993@gmail.com>
2024-04-23 11:53:32 +0200

    add more styles

--- src/SideBar.js
+++ src/SideBar.js
@@ -1,17 +1,32 @@
+import { BsPlusSquareFill, BsFillLightningFill, BsGearFill, BsPlus } from "react-icons/bs";
+import { FaFire, FaPoo } from "react-icons/fa";
+
 
 function SideBar() {
   return (
     <div className="fixed top-0 left-0 h-screen w-16 m-0 flex flex-col bg-gray-900 text-white shadow-lg">
-        <li>A</li>
-        <li>B</li>
-        <li>C</li>
-        <li>D</li>
-        <li>E</li>
-
-
+    
+        <SideBarIcon icon={<FaFire size="28" />} />
+        <SideBarIcon icon={<BsPlus size="32" />} />
+        <SideBarIcon icon={<BsFillLightningFill size="20" />} />
+        <SideBarIcon icon={<FaPoo size="20" />} />
+        <SideBarIcon icon={<BsGearFill size="20" />} />
 
     </div>
   )
+}
+
+function SideBarIcon ({ icon, text = 'tooltip' }) {
+return (
+    <div className="sidebar-icon group">
+{icon}
+
+<span class="sidebar-tooltip group-hover:scale-100">
+    {text}
+</span>
+    </div>
+)
+
 }
 
 export default SideBar
